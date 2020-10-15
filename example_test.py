07ad4feffc6818e874cbd0338867f0db4df7629b
Joseph <pikminzeldamarioguy@gmail.com>
2020-10-14 20:49:09 -0500

    4 tests
    2 pass 2 fail, mapped out everything

--- example_test.py
+++ example_test.py
@@ -35,7 +35,36 @@ def test_change_grade(grading_system):
     grading_system.usr.change_grade(username3,course,assignment,100)
     assert grading_system.users[username3]['courses'][course][assignment]['grade'] == 100
 
-#(4) 
+#(4) test staff assignment creation
+
+#(5) test add student by professor
+
+#(6) test drop student by professor
+
+#(7) test submit assignment for student
+
+#(8) test check if assignment is on time for student
+
+#(9) test check grades for student, fail
+def test_check_grade(grading_system):
+    grading_system.login(username3,password3)
+    grades = grading_system.usr.check_grades(course)
+    if 5 not in grades: #done this way so that order doesnt matter
+        assert False
+    if 3 not in grades:
+        assert False
+
+#(10) test view assignments for student
+
+#(11) test ability for student to directly message professor
+
+#(12) test for student ability to drop a class
+
+#(13) test for staff ability to email professor
+
+#(14) test for student ability to delete submission
+
+#(15) test for student ability to comment on submission
 
 @pytest.fixture
 def grading_system():
